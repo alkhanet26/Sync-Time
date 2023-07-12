daa6ccc0ccd28cacfbc7863e9f8791ff19a3a76a
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-07-12 23:39:42 +0700

    Create installtime.sh

---
+++ installtime.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Created by ALK-NET
+
+echo "Memasang Paket pendukung"
+opkg update && opkg install --force-reinstall adb curl libcurl
+clear
+echo "Mengunduh File Binaries"
+sleep 2
+wget -q "https://raw.githubusercontent.com/alkhanet26/Sync-Time/main/synctime.sh" -O /sbin/synctime && chmod +x /sbin/synctime
+echo "File Binaries sudah terpasang"
+echo "Pemasangan Sudah Selesai"
+sleep 3
+clear
