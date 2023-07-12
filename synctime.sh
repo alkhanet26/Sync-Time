a89e64721a20877127b0111147aa50f9aa423800
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-07-12 13:54:40 +0700

    Update synctime.sh

--- synctime.sh
+++ synctime.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+tldir='/tmp/date1'
 dtdir='/tmp/date'
 ipw=$(ip route | grep default | cut -d' ' -f3)
 
+echo "Mencari Info Waktu..."
+sleep 2
 # Ambil data jam di modem hilink
-{ echo "date"; sleep 3; } telnet ${ipw} > /tmp/date
+{ echo "date"; sleep 3; } | telnet ${ipw} > "$tldir"
+cat /tmp/date1 | grep GMT > "$dtdir"
 
-	day=$(cat "$dtdir" | cut -b 9-10)
+    day=$(cat "$dtdir" | cut -b 9-10)
     month=$(cat "$dtdir" | cut -b 5-7)
     year=$(cat "$dtdir" | cut -b 25-28)
     time=$(cat "$dtdir" | cut -b 12-19)
+    waktu=$(date)
     
     case $month in
         "Jan")
@@ -51,5 +56,11 @@ ipw=$(ip route | grep default | cut -d' ' -f3)
         *)
 
     esac
-	
-date -s -u $year.$month.$day-$time
+    
+echo "Menemukan Informasi Waktu...."
+sleep 1
+date -u -s $year.$month.$day-$time
+echo "Synchronize Waktu ke : "$waktu
+sleep 2
+echo "Sync time Completed"
+
