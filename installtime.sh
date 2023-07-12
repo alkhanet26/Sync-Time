8b58af1380f9287820d49cf61fba3d88139d55ca
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-07-13 01:24:26 +0700

    Update installtime.sh

--- installtime.sh
+++ installtime.sh
@@ -6,7 +6,7 @@ opkg update && opkg install --force-reinstall adb curl libcurl
 clear
 echo "Mengunduh File Binaries"
 sleep 2
-wget -q "https://raw.githubusercontent.com/alkhanet26/Sync-Time/main/synctime.sh" -O /sbin/synctime && chmod +x /sbin/synctime
+wget -q "https://raw.githubusercontent.com/alkhanet26/Sync-Time/main/synctime" -O /sbin/synctime && chmod +x /sbin/synctime
 echo "File Binaries sudah terpasang"
 echo "Pemasangan Sudah Selesai"
 sleep 3
